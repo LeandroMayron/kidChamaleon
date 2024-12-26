572bf8ccd0b26c689d17ae38b486d441d383eaf9
Leandro Mayron <leandromayron9999@gmail.com>
2024-12-26 15:33:35 -0300

    corrigido bugs dos cards

---
+++ script.js
@@ -0,0 +1,14 @@
+function mudarConteudo () {
+    document.getElementsByClassName("nomeFicha").innerText = "Red Stealth";
+    document.getElementsByClassName("listaFicha").innerText = "Samurai Haze";
+    document.getElementsByClassName("spanFicha").innerText = "(20 Diamantes)";
+    document.getElementsByClassName("textFicha").innerText = "faz os inimigos ficarem lentos"
+
+    document.getElementsByClassName("listaFicha2").innerText = "Diamond Snake";
+    document.getElementsByClassName("spanFicha2").innerText = "(50 Diamantes)";
+    document.getElementsByClassName("textFicha2").innerText = "uma serpente de diamantes dilacera os inimigos";
+
+    document.getElementsByClassName("descricaoFIcha").innerText = " também conhecido como ninjinha, samurai, vermelhinho. É um samurai dotado de dois principais movimentos: sua enorme espada para ataques diretos e seu enorme salto. Red ainda pode atacar para baixo quando pula, acertando desde inimigos até quebrando blocos no processo. É o personagem mais ágil do jogo, seu único contra é escorregar em ladeiras, não parando em pé por nada!"
+
+    document.getElementsByClassName("videoFicha").src = "./img/videoplayback.mp4"
+}
